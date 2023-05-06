4cd82ab94b29d70eaa53acd9ea0380960bb2e299
Karina Zaiets <karyz9611@gmail.com>
2023-05-06 21:21:03 +0100

    query for 2d requirement

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,11 +1,24 @@
 --Create SQL queries
 -- 1. Admins should be able to enable or disable the availability of a course 
+
 -- 2. Admins should be able to assign one or more courses to a teacher
+DELIMITER //
+CREATE PROCEDURE AssignCourses(IN CourseID_provided INT, IN TeacherID_provided INT)
+BEGIN
+UPDATE courses
+SET TeacherID = TeacherID_provided
+WHERE CourseID = CourseID_provided;
+END //
+
 -- 3. Students can browse and list all the available courses and see the course title and course teacher’s name.
+DELIMITER //
+CREATE PROCEDURE ViewCourses()
+BEGIN
 SELECT courses.Title 'Course Title', IFNULL(users.Name, 'Teacher Not Yet Assigned') AS `Teacher Name` 
 FROM courses
 LEFT JOIN users ON courses.TeacherID = users.UserID
 WHERE isAvailable = 1;
+END // 
 
 -- 4. Students can enrol in a course. Students should not be able to enrol in a course more than once at each time. 
 -- 5. Teachers can fail or pass a student.
