696c1d74dea279a4b9690ab3a9bc3764e9cfaa85
CT673 <108407338+CT673@users.noreply.github.com>
2023-05-14 18:49:13 +1000

    Error handling - line 35 to 48

--- server.js
+++ server.js
@@ -32,6 +32,22 @@ app.listen(port, () => {
 app.use(express.json());
 
 
+//Using default error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    error: 'Internal server error'
+  });
+});
+
+//Users accessing routes that are not defined
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: 'Not found'
+  });
+});
+
+
 // API EXAMPLES
 
 //defining variables to be passed to authorize procedure to make sure only authorized users perform action 
