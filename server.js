eddd9c6c810ec7d328249bc6cd8e62e309593803
g-m-desouza <g_m_desouza@hotmail.com>
2023-05-15 13:43:00 +0100

    sever name correct

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE_NAME
 });
 
 //instance expression
