0c01d7638d15894278e0f913a159f483973c80e0
Karina Zaiets <karyz9611@gmail.com>
2023-05-19 18:45:00 +0200

    beautify the code

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
-// test
-const e = require('express');
 const express = require('express');
-const mysql=require('mysql2');
+const mysql = require('mysql2');
 const dotenv = require('dotenv')
 dotenv.config()
 
-//server
+// Connecting to database
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
-//instance expression
+// Instance expression
 const app = express();
 
 //log
 console.log('test');
 
-//server
+// Server
 const port = process.env.PORT;
 
 //Start server listening on the port
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+    console.log(`listening on port ${port}`)
 });
 
 
-//json 
+// json 
 app.use(express.json());
 
 
@@ -42,194 +40,183 @@ AdminAuthorized = 'Admin'
 
 // API for student enrolling to course
 app.post('/enrol/:userid/:courseid/', function (request, response) {
-    //Check if a user is authorized to perform an action
+    // Check if a user is authorized to perform an action
     // Call the Authorize procedure
     connection.query(`CALL Authorize(${+request.params.userid}, '${StudentAuthorized}');`, (error, result) => {
-      if (error) {
-        console.log(error);
-      }
-      const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-      if (authorisation_response !== 'OK') {
-        response.send(authorisation_response);
-      }else{
-        connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
-            if (error) {
-                console.log(error);
-              }
-            coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-            if (coursecheck_response !== 'OK') {
-                response.send(coursecheck_response);
-            }
-            else{
-    // Call the stored procedure and pass the necessary parameters received from the user
-    connection.query(`CALL Enrol(${+request.params.courseid}, ${+request.params.userid});`, (error, result)=>{
-        // Handle SQL errors
         if (error) {
-           console.log(error)
-        };
-        // Get confirmation that enrollment was successful/unsuccessful from the database
-        message = JSON.parse(JSON.stringify(result[0]))[0].Response
-  
-        // Send the message to the user
-        response.send(message)
-      });
-    }
-});
-   }
-  });
+            console.log(error);
+        }
+        const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+        if (authorisation_response !== 'OK') {
+            response.send(authorisation_response);
+        } else {
+            // Check if the course exists
+            connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                if (coursecheck_response !== 'OK') {
+                    response.send(coursecheck_response);
+                } else {
+                    // Call the stored procedure and pass the necessary parameters received from the user
+                    connection.query(`CALL Enrol(${+request.params.courseid}, ${+request.params.userid});`, (error, result) => {
+                        // Handle SQL errors
+                        if (error) {
+                            console.log(error)
+                        };
+                        // Get confirmation that enrollment was successful/unsuccessful from the database
+                        message = JSON.parse(JSON.stringify(result[0]))[0].Response
+
+                        // Send the message to the user
+                        response.send(message)
+                    });
+                }
+            });
+        }
+    });
 });
 
 
 
 // API for students to view available courses
 app.get('/courses/:userid', function (request, response) {
-   connection.query(`CALL Authorize(${+request.params.userid}, '${StudentAuthorized}');`, (error, result) => {
-      if (error) {
-        console.log(error);
-      }
-      //Check if a user is authorized to perform an action
-      // Call the Authorize procedure
-      const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-      if (authorisation_response !== 'OK') {
-        response.send(authorisation_response);
-      }else{
-    // Call the stored procedure to get list of courses
-    connection.query(`CALL ViewCourses`, (error, result)=>{
-        // Handle SQL errors
+    connection.query(`CALL Authorize(${+request.params.userid}, '${StudentAuthorized}');`, (error, result) => {
         if (error) {
-           console.log(error)
-        };
+            console.log(error);
+        }
+        // Check if a user is authorized to perform an action
+        // Call the Authorize procedure
+        const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+        if (authorisation_response !== 'OK') {
+            response.send(authorisation_response);
+        } else {
+            // Call the stored procedure to get list of courses
+            connection.query(`CALL ViewCourses`, (error, result) => {
+                // Handle SQL errors
+                if (error) {
+                    console.log(error)
+                };
 
-        // Get the list of available courses sent out of the database from the json body
-       message = result[0]
+                // Get the list of available courses sent out of the database from the json body
+                message = result[0]
 
-        // Send the message to the user
-        response.send(message)
-      });
-   }
- });
+                // Send the message to the user
+                response.send(message)
+            });
+        }
+    });
 });
-
 
 
 
 //API for admins to make courses available/unavailable
 app.post('/courseavail/:userid/:courseid/:enabledisable', function (request, response) {
-  //Check if a user is authorized to perform an action
-  connection.query(`CALL Authorize(${+request.params.userid}, '${AdminAuthorized}');`, (error, result) => {
-   if (error) {
-     console.log(error);
-   }
-   //Check if a user is authorized to perform an action
-   // Call the Authorize procedure
-   const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-   if (authorisation_response !== 'OK') {
-     response.send(authorisation_response);
-   }else{
-       connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
+    //Check if a user is authorized to perform an action
+    connection.query(`CALL Authorize(${+request.params.userid}, '${AdminAuthorized}');`, (error, result) => {
         if (error) {
             console.log(error);
-          }
-        coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-        if (coursecheck_response !== 'OK') {
-            response.send(coursecheck_response);
         }
-        else{
-    // Call the stored procedure to enable/disable course
-      connection.query(`CALL ChangeAvailability(${+request.params.courseid}, '${request.params.enabledisable.toLowerCase()}');`, (error, result)=>{
-      if (error) {
-          console.log(error);
-         }
-   
-         message = JSON.parse(JSON.stringify(result[0]))[0].Response;
-         response.send(message);
-      });
-    }
+        // Check if a user is authorized to perform an action
+        // Call the Authorize procedure
+        const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+        if (authorisation_response !== 'OK') {
+            response.send(authorisation_response);
+        } else {
+            // Check if the course exists
+            connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                if (coursecheck_response !== 'OK') {
+                    response.send(coursecheck_response);
+                } else {
+                    // Call the stored procedure to enable/disable course
+                    connection.query(`CALL ChangeAvailability(${+request.params.courseid}, '${request.params.enabledisable.toLowerCase()}');`, (error, result) => {
+                        if (error) {
+                            console.log(error);
+                        }
+                        message = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                        response.send(message);
+                    });
+                }
+            });
+        }
+    });
 });
-     }
-   });
-});
-
-
 
 
 
 //API for admins assigning teachers to courses
 app.post('/assignteacher/:userid/:courseid/:teacherid', function (request, response) {
-   //Check if a user is authorized to perform an action
-   // Call the Authorize procedur
-   connection.query(`CALL Authorize(${+request.params.userid}, '${AdminAuthorized}');`, (error, result) => {
-      if (error) {
-        console.log(error);
-      }
-      const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-      if (authorisation_response !== 'OK') {
-        response.send(authorisation_response);
-      }else{
-        // Check if the course exists
-        connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
-            if (error) {
-                console.log(error);
-              }
-            coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-            if (coursecheck_response !== 'OK') {
-                response.send(coursecheck_response);
-            }
-            else{
-
-  // Call the procedure to assign course to teacher
-   connection.query(`CALL AssignCourses(${+request.params.courseid}, ${+request.params.teacherid});`, (error, result)=>{
-      if (error) {
-         console.log(error);
-       }
-       message = JSON.parse(JSON.stringify(result[0]))[0].Response;
-       response.send(message);
-     });
-    }
+    //Check if a user is authorized to perform an action
+    // Call the Authorize procedur
+    connection.query(`CALL Authorize(${+request.params.userid}, '${AdminAuthorized}');`, (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        const authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+        if (authorisation_response !== 'OK') {
+            response.send(authorisation_response);
+        } else {
+            // Check if the course exists
+            connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                if (coursecheck_response !== 'OK') {
+                    response.send(coursecheck_response);
+                } else {
+                    // Call the procedure to assign course to teacher
+                    connection.query(`CALL AssignCourses(${+request.params.courseid}, ${+request.params.teacherid});`, (error, result) => {
+                        if (error) {
+                            console.log(error);
+                        }
+                        message = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                        response.send(message);
+                    });
+                }
+            });
+        }
     });
-    }
-   });
- });
-
+});
 
 
 
 //API for teachers to fail or pass a student.
 app.post('/mark/:userid/:courseid/:studentid/:markgiven', function (request, response) {
-   //Check if a user is authorized to perform an action
-   // Call the Authorize procedure
-   connection.query(`CALL Authorize(${+request.params.userid}, '${TeacherAuthorized}');`, (error, result) => {
-    if (error) {
-      console.log(error);
-    }
-    authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-    if (authorisation_response !== 'OK') {
-      response.send(authorisation_response);
-    }else{
-        // Check if the course exists
-        connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
-            if (error) {
-                console.log(error);
-              }
-            coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
-            if (coursecheck_response !== 'OK') {
-                response.send(coursecheck_response);
-            }
-            else{
-                // Call the procedure to grade students
-                connection.query(`CALL MarkStudents(${+request.params.courseid}, ${+request.params.studentid}, '${request.params.markgiven.toLowerCase()}');`, (error, result) => {
-                    if (error) {
-                      console.log(error);
-                    }
-                    message = JSON.parse(JSON.stringify(result[0]))[0].Response;
-                    response.send(message);
-                  });
-            }
+    //Check if a user is authorized to perform an action
+    // Call the Authorize procedure
+    connection.query(`CALL Authorize(${+request.params.userid}, '${TeacherAuthorized}');`, (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        authorisation_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+        if (authorisation_response !== 'OK') {
+            response.send(authorisation_response);
+        } else {
+            // Check if the course exists
+            connection.query(`CALL CheckCourse(${+request.params.courseid});`, (error, result) => {
+                if (error) {
+                    console.log(error);
+                }
+                coursecheck_response = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                if (coursecheck_response !== 'OK') {
+                    response.send(coursecheck_response);
+                } else {
+                    // Call the procedure to grade students
+                    connection.query(`CALL MarkStudents(${+request.params.courseid}, ${+request.params.studentid}, '${request.params.markgiven.toLowerCase()}');`, (error, result) => {
+                        if (error) {
+                            console.log(error);
+                        }
+                        message = JSON.parse(JSON.stringify(result[0]))[0].Response;
+                        response.send(message);
+                    });
+                }
 
+            });
+        }
     });
-   }
-  });
 });
-
-
- 
--- sql_queries.sql
+++ sql_queries.sql
@@ -1,26 +1,30 @@
--- Create SQL queries
+-- SQL queries
 -- 1. Admins should be able to enable or disable the availability of a course 
 DROP PROCEDURE ChangeAvailability;
-DELIMITER //
-CREATE PROCEDURE ChangeAvailability(IN CourseID_provided INT, IN Availability_provided VARCHAR(10))
+DELIMITER / / 
+CREATE PROCEDURE ChangeAvailability(IN CourseID_provided INT, IN Availability_provided VARCHAR(10)) 
 BEGIN
 DECLARE TeacherCheck INT;
 DECLARE EnableDisable INT;
 DECLARE Response VARCHAR(100);
-SET TeacherCheck = (SELECT TeacherID FROM courses WHERE CourseID = CourseID_provided);
+SET TeacherCheck = (
+        SELECT TeacherID
+        FROM courses
+        WHERE CourseID = CourseID_provided
+    );
 -- Set the value to be entered to the database to 0 if user inputs "disable" and 1 if "enable"
-SET EnableDisable = CASE Availability_provided
-	WHEN 'disable' THEN 0
-	WHEN 'enable' THEN 1
-	ELSE NULL
+SET EnableDisable = CASE
+        Availability_provided
+        WHEN 'disable' THEN 0
+        WHEN 'enable' THEN 1
+        ELSE NULL
     END;
 -- Check if a user entered invalid value
-IF EnableDisable IS NULL
-THEN
+IF EnableDisable IS NULL THEN
 SET Response = 'Please, either enter "enable" or "disable"';
 -- Check if teacher already assigned when admin tries to enable the course
-ELSE IF TeacherCheck = 0 AND EnableDisable = 1
-THEN
+ELSE IF TeacherCheck = 0
+AND EnableDisable = 1 THEN
 SET Response = "Assign a teacher to the course first.";
 ELSE
 UPDATE courses
@@ -31,24 +35,30 @@ SET Response = "Success!";
 END IF;
 END IF;
 SELECT Response;
-END //
+END / / 
 
 
 -- 2. Admins should be able to assign one or more courses to a teacher
 DROP PROCEDURE IF EXISTS AssignCourses;
-DELIMITER //
-CREATE PROCEDURE AssignCourses(IN CourseID_provided INT, IN TeacherID_provided INT)
+DELIMITER / / 
+CREATE PROCEDURE AssignCourses(IN CourseID_provided INT, IN TeacherID_provided INT) 
 BEGIN
 DECLARE Response VARCHAR(100);
 DECLARE TeacherCheck INT;
-SET TeacherCheck = (SELECT RoleID FROM users WHERE UserID = TeacherID_provided);
+SET TeacherCheck = (
+        SELECT RoleID
+        FROM users
+        WHERE UserID = TeacherID_provided
+    );
 -- Check if a teacher exists in the system
-IF NOT EXISTS (SELECT 1 FROM users WHERE UserID = TeacherID_provided) 
-THEN
+IF NOT EXISTS (
+    SELECT 1
+    FROM users
+    WHERE UserID = TeacherID_provided
+) THEN
 SET Response = "This teacher doesn't exist.";
 -- Make sure a TeacherID provided belongs to a teacher
-ELSE IF TeacherCheck != 2
-THEN
+ELSE IF TeacherCheck != 2 THEN
 SET Response = "This user is not a teacher";
 ELSE
 UPDATE courses
@@ -59,99 +69,124 @@ SET Response = "Success.";
 END IF;
 END IF;
 SELECT Response;
-END //
+END / / 
+
 
 -- 3. Students can browse and list all the available courses and see the course title and course teacher’s name.
-DROP PROCEDURE IF EXISTS ViewCourses; 
-DELIMITER //
-CREATE PROCEDURE ViewCourses()
-BEGIN
+DROP PROCEDURE IF EXISTS ViewCourses;
+DELIMITER / / 
+CREATE PROCEDURE ViewCourses() 
+BEGIN 
 -- Perform join
-SELECT courses.Title 'Course Title', IFNULL(users.Name, 'Teacher Not Yet Assigned') AS `Teacher Name` 
+SELECT courses.Title 'Course Title',
+    IFNULL(users.Name, 'Teacher Not Yet Assigned') AS `Teacher Name`
 FROM courses
-LEFT JOIN users ON courses.TeacherID = users.UserID
--- Make sure students see only available courses
+    LEFT JOIN users ON courses.TeacherID = users.UserID -- Make sure students see only available courses
 WHERE isAvailable = 1;
-END // 
+END / / 
+
 
 -- 4. Students can enrol in a course. Students should not be able to enrol in a course more than once at each time.
-DROP PROCEDURE IF EXISTS Enrol; 
-DELIMITER //
-CREATE PROCEDURE Enrol(IN CourseID_provided INT, IN StudentId_provided INT)
+DROP PROCEDURE IF EXISTS Enrol;
+DELIMITER / / 
+CREATE PROCEDURE Enrol(IN CourseID_provided INT, IN StudentId_provided INT) 
 BEGIN
 DECLARE EnrolmentExists INT;
 DECLARE Response VARCHAR(100);
-SET EnrolmentExists = (SELECT EnrolmentID FROM enrolments WHERE CourseID = CourseID_provided AND UserID = StudentId_provided);
+SET EnrolmentExists = (
+        SELECT EnrolmentID
+        FROM enrolments
+        WHERE CourseID = CourseID_provided
+            AND UserID = StudentId_provided
+    );
 -- Make sure a student can't enrol more then once
-IF EnrolmentExists IS NOT NULL
-	THEN 
-    SET Response = "The student is already enrolled.";
+IF EnrolmentExists IS NOT NULL THEN
+SET Response = "The student is already enrolled.";
 ELSE
-	INSERT INTO enrolments (CourseId, UserId)
-    VALUES (CourseID_provided, StudentId_provided);
+INSERT INTO enrolments (CourseId, UserId)
+VALUES (CourseID_provided, StudentId_provided);
 -- Check if enrollment successfully generated
-		IF ROW_COUNT() > 0
-        THEN
--- Return success message
-		SET Response = "Enrolment successful.";
-END IF;        
+IF ROW_COUNT() > 0 THEN -- Return success message
+SET Response = "Enrolment successful.";
+END IF;
 END IF;
 SELECT Response;
-END //
+END / / 
+
 
 -- 5. Teachers can fail or pass a student.
 DROP PROCEDURE MarkStudents;
-DELIMITER //
-CREATE PROCEDURE MarkStudents(IN CourseID_provided INT, IN StudentId_provided INT, IN Mark_provided VARCHAR(10))
+DELIMITER / / 
+CREATE PROCEDURE MarkStudents(IN CourseID_provided INT, IN StudentId_provided INT, IN Mark_provided VARCHAR(10)) 
 BEGIN
 DECLARE EnrolmentCheck INT;
 DECLARE Mark_ INT;
 DECLARE Response VARCHAR(100);
-SET EnrolmentCheck = (SELECT EnrolmentID FROM enrolments WHERE CourseID = CourseID_provided AND UserID = StudentId_provided);
+SET EnrolmentCheck = (
+        SELECT EnrolmentID
+        FROM enrolments
+        WHERE CourseID = CourseID_provided
+            AND UserID = StudentId_provided
+    );
 -- Set the value to be entered to the database to 0 if user inputs "fail" and 1 if "pass"
-SET Mark_ = CASE Mark_provided
-	WHEN 'fail' THEN 0
-	WHEN 'pass' THEN 1
-	ELSE NULL
+SET Mark_ = CASE
+        Mark_provided
+        WHEN 'fail' THEN 0
+        WHEN 'pass' THEN 1
+        ELSE NULL
     END;
--- check if enrollment exists
-IF EnrolmentCheck IS NULL
-	THEN SET Response = "The student does not have an enrolment for this course.";
+-- Check if enrollment exists
+IF EnrolmentCheck IS NULL THEN
+SET Response = "The student does not have an enrolment for this course.";
 -- Check if a user entered invalid value for a mark
-ELSE IF Mark_ IS NULL
-	THEN
-    SET Response = 'Please, provide either "pass" or "fail" for grading';
+ELSE IF Mark_ IS NULL THEN
+SET Response = 'Please, provide either "pass" or "fail" for grading';
 ELSE
-	UPDATE enrolments
-	SET Mark = Mark_
-	WHERE CourseID = CourseID_provided AND UserID = StudentId_provided;
+UPDATE enrolments
+SET Mark = Mark_
+WHERE CourseID = CourseID_provided
+    AND UserID = StudentId_provided;
 -- Return success message
-    SET Response = "Grading successful.";
+SET Response = "Grading successful.";
 END IF;
 END IF;
 SELECT Response;
-END //
+END / / 
 
 
 -- Ensure only the authorized access can perform an action.
 DROP PROCEDURE IF EXISTS Authorize;
-DELIMITER //
-CREATE PROCEDURE Authorize(IN UserID_provided INT, IN Role_provided VARCHAR(20))
+DELIMITER / / 
+CREATE PROCEDURE Authorize(IN UserID_provided INT, IN Role_provided VARCHAR(20)) 
 BEGIN
 DECLARE UserCheck INT;
 DECLARE UserRole INT;
 DECLARE RoleCheck INT;
 DECLARE Response VARCHAR(100);
-SET UserCheck = (SELECT UserID FROM users WHERE UserID = UserID_provided);
-SET UserRole = (SELECT RoleID FROM roles WHERE Role = Role_provided);
-SET RoleCheck = (SELECT RoleID FROM users WHERE UserID = UserID_provided);
+SET UserCheck = (
+        SELECT UserID
+        FROM users
+        WHERE UserID = UserID_provided
+    );
+SET UserRole = (
+        SELECT RoleID
+        FROM roles
+        WHERE Role = Role_provided
+    );
+SET RoleCheck = (
+        SELECT RoleID
+        FROM users
+        WHERE UserID = UserID_provided
+    );
 -- Check if the user id exists in the system
-IF NOT EXISTS (SELECT 1 FROM users WHERE UserID = UserID_provided)
-THEN 
+IF NOT EXISTS (
+    SELECT 1
+    FROM users
+    WHERE UserID = UserID_provided
+) THEN
 SET Response = "This user doesn't exist";
 -- Check if the user role corresponds to user role needed to perform a certain action
-ELSE IF RoleCheck != UserRole
-THEN 
+ELSE IF RoleCheck != UserRole THEN
 SET Response = "This user is not authorized to perform the action";
 ELSE 
 -- Return success message
@@ -159,23 +194,27 @@ SET Response = 'OK';
 END IF;
 END IF;
 SELECT Response;
-END //
+END / / 
+
 
 -- Check if a course exists for apis that rely on user inputing a course
 DROP PROCEDURE IF EXISTS CheckCourse;
-DELIMITER //
-CREATE PROCEDURE CheckCourse(IN CourseID_provided INT)
+DELIMITER / / 
+CREATE PROCEDURE CheckCourse(IN CourseID_provided INT) 
 BEGIN
 DECLARE CourseExists INT;
 DECLARE Response VARCHAR(100);
-SET CourseExists = (SELECT CourseID FROM courses WHERE CourseID = CourseID_provided);
+SET CourseExists = (
+        SELECT CourseID
+        FROM courses
+        WHERE CourseID = CourseID_provided
+    );
 -- Make sure a course exists in the system
-IF CourseExists IS NULL
-THEN
+IF CourseExists IS NULL THEN
 SET Response = "This course doesn't exist.";
 ELSE 
 -- Return success message
 SET Response = "OK";
 END IF;
 SELECT Response;
-END//
+END / /
